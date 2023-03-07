74cc5679149d868338af3741f6c005e69946ff5b
Paragonsean <cos30degrees@gmail.com>
2023-03-07 12:00:00 -0500

    Generated Python client and docs for 1.0.0

--- 1.0.0/java-client/src/main/java/org/openapitools/client/model/Getcompanymatch200Response.java
+++ 1.0.0/java-client/src/main/java/org/openapitools/client/model/Getcompanymatch200Response.java
@@ -48,7 +48,7 @@ import org.openapitools.client.JSON;
 /**
  * Getcompanymatch200Response
  */
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2024-10-11T02:00:20.649519-04:00[America/New_York]", comments = "Generator version: 7.9.0")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2024-10-11T02:09:55.696093-04:00[America/New_York]", comments = "Generator version: 7.9.0")
 public class Getcompanymatch200Response {
   public static final String SERIALIZED_NAME_CODE = "Code";
   @SerializedName(SERIALIZED_NAME_CODE)
